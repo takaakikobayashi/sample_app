77c05b0da2870544643b8f9610a0109d5f77abf7
vagrant <takakoba0426@gmail.com>
2020-03-23 04:22:27 +0000

    [Start]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   get "todolists/:id" => "todolists#show", as: "todolist"
 
   get "todolists/:id/edit" => "todolists#edit", as: "edit_todolist"
+
+  patch "todolist/:id" => "todolists#update", as: "update_todolist"
   
 end
